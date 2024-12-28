27c3a56b5d70b858428bf4b121b39e89e46861e0
Rohit-GHacc <rohitchugh142001@gmail.com>
2024-12-29 00:48:53 +0530

    trying trying

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -10,14 +10,15 @@ import useGetTweets from '../hooks/useGetTweets'
 function Home() {
     const navigate = useNavigate()
     const {user} = useSelector(store=>store.user)
-    useGetTweets(user?._id)
-    useOtherUsers(user?._id)
+    console.log()
     useEffect(()=>{
         if(!user){
             navigate('/login')
         }
-            //eslint-disable-next-line
+        //eslint-disable-next-line
     },[])
+    useGetTweets(user?._id)
+    useOtherUsers(user?._id)
     return (
         <div>
             <div className='flex mx-auto w-[80%] justify-center gap-[2%]'>
