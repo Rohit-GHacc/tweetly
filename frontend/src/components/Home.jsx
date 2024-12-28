47be84e6d0e993c3c1764252a2d4dda25b0b834c
Rohit-GHacc <rohitchugh142001@gmail.com>
2024-12-29 00:37:45 +0530

    hmmm

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 import LeftSideBar from './LeftSideBar'
 import RightSideBar from './RightSideBar'
 import { Outlet, useNavigate } from 'react-router-dom'
@@ -10,12 +10,12 @@ import useGetTweets from '../hooks/useGetTweets'
 function Home() {
     const navigate = useNavigate()
     const {user} = useSelector(store=>store.user)
-    React.useEffect(()=>{
+    useGetTweets(user?._id)
+    useOtherUsers(user?._id)
+    useEffect(()=>{
         if(!user){
             navigate('/login')
         }
-        useGetTweets(user?._id)
-        useOtherUsers(user?._id)
             //eslint-disable-next-line
     },[])
     return (
